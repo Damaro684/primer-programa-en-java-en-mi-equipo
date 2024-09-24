4dda81e652e9a3bd4a6f2dfb7b20a1ecc9aaa070
Daniel <danielmateos684@gmail.com>
2024-09-24 09:06:20 +0200

    Mi cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]){
 	
-		System.out.println("Mateos Rodríguez");
 		System.out.println("18 años, 2006");
 		System.out.println("Alitas de pollo");
+		System.out.println("Trueno");
 
 	}
 
